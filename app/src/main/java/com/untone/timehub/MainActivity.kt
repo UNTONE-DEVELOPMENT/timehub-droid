3c45ddfe4a4255ab0fdff1c320b623c703e19f2a
Matthew <43993777+matthew-5pl@users.noreply.github.com>
2021-04-29 15:08:16 +0200

    add blank prog and project info

---
+++ app/src/main/java/com/untone/timehub/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.untone.timehub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
